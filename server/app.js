1c4754f2091ac68d704ee1487e5a98cb845a1d6f
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-17 20:04:17 +0700

    added bcrypt, cors, mongoose, and dotenv packages, as well as initialized mongoose database

--- server/app.js
+++ server/app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+mongoose.connect("mongodb://localhost:27017/socmed-aggregator", {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("mongoose connected!");
+});
 
 var app = express();
 
@@ -13,6 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
