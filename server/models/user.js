3881c98e75347ca296a06cc3ab2a91a16d1d43ea
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-17 20:55:46 +0700

    finished user schema and added oauth and jwt packages

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema
+const bcrypt = require('bcrypt');
+const schema = mongoose.Schema;
 
 const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
   name: String,
-  email: String,
   fbid: String,
   password: String,
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
 })
 
 const User = mongoose.model("User", userSchema);
