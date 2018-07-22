0984b2b5ced605c82391d651de9c6885af15d9fe
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-22 16:33:39 +0700

    added cdn's

--- client/lib/main.js
+++ client/lib/main.js
@@ -1,78 +1,19 @@
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
 
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
-  if (response.status === 'connected') {
-    const authTokenFB = response.authResponse.accessToken;
 
-    axios.post("http://localhost:3000/users/loginWithFB", {
-        token: authTokenFB,
-      })
-      .then(function(res) {
-        localStorage.userJwt = res.data;
-          window.location.href = "http://localhost:8080/dashboard.html";
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
 
-  } else {
-    // The person is not logged into your app or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  }
-}
+var vm = new Vue({
+  el: '#app',
+  data: {
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '463129100825104',
-    cookie     : true,  // enable cookies to allow the server to access
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  // FB.getLoginStatus(function(response) {
-  //   statusChangeCallback(response);
-  // });
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  },
+  methods: {
+    tweet() {
+      let caption = 'justaratherfancyToDo is fancy indeed, check it out!';
+      let addrress = `https://twitter.com/intent/tweet?text=${caption}`;
+      window.location = addrress;
+    },
+  },
+});
