6b88b90ddd946b65b603c9dd69a80c441f7a30b1
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-23 20:37:43 +0700

    restructured authentication.js for reusability

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,17 +1,22 @@
 var jwt = require('jsonwebtoken');
 
-function verifyToken(req, res, next) {
-  const token = req.headers.token;
-  jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
-      if (err) {
-        res
-          .status(400)
-          .send(err);
-      } else {
-        next();
+class authentication {
+  static verifyToken(req, res, next) {
+    const token = req.headers.token;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+        if (err) {
+          res
+            .status(400)
+            .send(err);
+        } else {
+          next();
+        }
       }
-    }
-  );
+    );
+  }
+
+
 }
 
-module.exports = verifyToken;
+
+module.exports = authentication;
